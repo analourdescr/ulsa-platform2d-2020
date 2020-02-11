bea7bb7cec6657d97514be17d0eb9ae0e918bf01
unknown <Helliud>
2020-02-10 20:23:01 -0700

    Con algunos errores

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+   [SerializeField]
+   float moveSpeed = 3f;
+   [SerializeField]
+   float delay;
+   float timer;
+
+    [SerializeField]
+
+    Vector2 dir; //Direction
+
+
+   void Update()
+   {
+       transform.Translate(dir * moveSpeed * Time.deltaTime);
+
+        timer += Time.deltaTime;
+
+        if(timer >= delay)
+        {
+            timer = 0f;
+            dir = dir == Vector2.right ? Vector2.left : Vector2.right;
+        }
+   }
+}
---
+++ Assets/Scripts/GameplaySystem.cs
@@ -0,0 +1,48 @@
+namespace Platform2DUtils.GameplaySystem
+{
+    using UnityEngine;
+
+    public class GameplaySystem
+    {
+        ///<summary>
+        /// Returns a Vector2 with Horizontal and Vertical axes.
+        ///</summary>
+        public static Vector2 Axis
+        {
+            get => new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        }
+
+        ///<summary>
+        /// Moves player in Horizontal axis with keyboard inputs.
+        ///</summary>
+        ///<param name="t">Transform component of the player</param>
+        ///<param name="moveSpeed">The coeficient of speed</param>
+        public static void TMovement(Transform t, float moveSpeed)
+        {
+            t.Translate(Vector2.right * Axis.x * moveSpeed);
+        }
+        
+        ///<summary>
+        /// Moves player in Horizontal axis with keyboard inputs and multiplied by delta time.
+        ///</summary>
+        ///<param name="t">Transform component of the player</param>
+        ///<param name="moveSpeed">The coeficient of speed</param>
+        public static void TMovementDelta(Transform t, float moveSpeed)
+        {
+            t.Translate(Vector2.right * Axis.x * moveSpeed * Time.deltaTime);
+        }
+
+        ///<summary>
+        /// Returns if jump button was buttondown.
+        ///</summary>
+        public static bool JumpBtn
+        {
+            get => Input.GetButtonDown("Jump");
+        }
+
+        public static void Jump(Rigidbody2D rb2D, float jumpForce)
+        {
+            rb2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+}
